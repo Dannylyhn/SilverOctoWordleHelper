8a4ae57245a9fce63511854770746e0a4bd93bfd
Danny <dannylyhn@gmail.com>
2023-08-25 12:18:23 +0200

    Map mockdata to table

--- src/components/ListResult/ListResult.jsx
+++ src/components/ListResult/ListResult.jsx
@@ -1,9 +1,23 @@
-export default function ListResult(){
+export default function ListResult(result){
 
+    const mockData = ["Price", "Canes", "Rises"]
 
     return (
         <div>
-            Hello
+            <table>
+                <thead>
+                    <tr>
+                    <th>Possible solutions:</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {mockData.map((stringValue, index) => (
+                    <tr key={index}>
+                        <td>{stringValue}</td>
+                    </tr>
+                    ))}
+            </tbody>
+    </table>
         </div>
     );
 }
