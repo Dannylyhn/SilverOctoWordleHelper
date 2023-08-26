73139a29de5c98484f073aeec402668ad030e92f
Danny <dannylyhn@gmail.com>
2023-08-26 15:33:06 +0200

    Rewrite code to auto focus input fields

--- src/components/LetterInputs/LetterInputs.jsx
+++ src/components/LetterInputs/LetterInputs.jsx
@@ -4,52 +4,41 @@ import React from 'react';
 
 
 const LetterInputs = (props) => {
+  const inputStyle = "text-center w-10 rounded-lg px-1 py-1 mr-1 border focus:outline-none focus:ring-2 focus:ring-blue-500 mb-4";
+  const maxLength = 1;
 
-   
-    const inputStyle = "text-center w-10 rounded-lg px-1 py-1 mr-1 border focus:outline-none focus:ring-2 focus:ring-blue-500 mb-4"
-    const maxLength = 1;
-    
-    const [letter, setLetter] = useState("");
-    const [letter2, setLetter2] = useState("");
-    const [letter3, setLetter3] = useState("");
-    const [letter4, setLetter4] = useState("");
-    const [letter5, setLetter5] = useState("");
+  const inputRefs = useRef([]);
+  
+  const [letters, setLetters] = useState(["", "", "", "", ""]);
 
-    const handleLetterchange = (e) => {
-      setLetter(e.target.value.toLocaleLowerCase());
-      console.log(e.target.value);
-    }
-    const handleLetter2change = (e) => {
-      setLetter2(e.target.value.toLocaleLowerCase());
-      console.log(e.target.value);
-    }
-    const handleLetter3change = (e) => {
-      setLetter3(e.target.value.toLocaleLowerCase());
-      console.log(e.target.value);
-    }
-    const handleLetter4change = (e) => {
-      setLetter4(e.target.value.toLocaleLowerCase());
-      console.log(e.target.value);
-    }
-    const handleLetter5change = (e) => {
-      setLetter5(e.target.value.toLocaleLowerCase());
-      console.log(e.target.value);
-    }
+  const handleLetterChange = (index, e) => {
+    const newLetters = [...letters];
+    newLetters[index] = e.target.value.toLowerCase();
+    setLetters(newLetters);
 
-    useEffect(() => {
-      let result = letter + letter2 +letter3 +letter4+ letter5;
-      props.dataToParent(result);
-    },[letter, letter2, letter3, letter4, letter5]);
+    if (e.target.value.length >= maxLength && index < letters.length - 1) {
+      inputRefs.current[index + 1].focus();
+    }
+  };
 
-    return (
-      <div>
-        <input onChange = {handleLetterchange}  maxLength={maxLength} value = {letter} className={inputStyle}></input>
-        <input onChange = {handleLetter2change} maxLength={maxLength} value = {letter2} className={inputStyle}></input>
-        <input onChange = {handleLetter3change} maxLength={maxLength} value = {letter3} className={inputStyle}></input>
-        <input onChange = {handleLetter4change} maxLength={maxLength} value = {letter4} className={inputStyle}></input>
-        <input onChange = {handleLetter5change} maxLength={maxLength} value = {letter5} className={inputStyle}></input>
-      </div>
-    )
+  useEffect(() => {
+    const result = letters.join("");
+    props.dataToParent(result);
+  }, [letters]);
 
+  return (
+    <div>
+      {letters.map((letter, index) => (
+        <input
+          key={index}
+          ref={element => inputRefs.current[index] = element}
+          onChange={(e) => handleLetterChange(index, e)}
+          maxLength={maxLength}
+          value={letter}
+          className={inputStyle}
+        />
+      ))}
+    </div>
+  );
   }
   export default LetterInputs;
