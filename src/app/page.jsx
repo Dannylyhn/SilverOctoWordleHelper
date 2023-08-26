00afd781e10cd1e15713582ac9427c19a1a06e27
Danny <dannylyhn@gmail.com>
2023-08-26 15:40:44 +0200

    Fix escaped aprostrophes

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -88,7 +88,7 @@ export default function Home() {
      <div className="mt-20 h-screen w-screen text-center">
         <h1 className="mt-4 mb-4 text-3xl font-extrabold text-gray-900 dark:text-white md:text-5xl lg:text-6xl"><span className="text-transparent bg-clip-text bg-gradient-to-r to-emerald-600 from-sky-400">SilverOctoWordleSolver</span></h1>
         <h4 className="text-lg font-bold text-black-500 lg:text-xl dark:text-gray-400">Input correct letters at the correct spaces</h4>
-        <h4>Please use '_' at the missing spaces</h4>
+        <h4>Please use &apos;_&apos; at the missing spaces</h4>
         <LetterInputs dataToParent={setCorrectPlacedLetters} />
         <h4>Input valid letters at incorrect spaces</h4>
         <LetterInputs dataToParent={setincorrectPlacedLetters} />
